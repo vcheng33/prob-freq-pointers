55b77347dac481739b0730fe1cefb2e04bcc98aa
Jenny Liu <jennyliuye@gmail.com>
2021-08-27 16:12:01 -0700

    finished average pair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,29 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+/* 
+function should accept an ordered array and a target average number, 
+determin whether there is a pair of values in array that average to the target average number.
+returns a boolean
+
+-if input array is empty, return false
+
+-- set left and right pointer at beginning and end of array
+- if average of left and right greater than target, move right pointer to the left
+- if average of left and right smaller than target, move left pointer to the right
+-if equal, return true;
+ */
+function averagePair(nums, average) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (right > left) {
+        if ((nums[left] + nums[right]) / 2 > average) {
+            right -= 1;
+        }
+        if ((nums[left] + nums[right]) / 2 < average) {
+            left += 1;
+        }
+        if ((nums[left] + nums[right]) / 2 === average) {
+            return true;
+        }
+    }
+    return false;
 }

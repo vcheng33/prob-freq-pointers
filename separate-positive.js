0fcfe0d2a13ae87fa3b95e9735135e84f93ef975
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-08-27 17:12:25 -0700

    two-array-object.js completed

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,19 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(array) {
+
+    // left = 0
+    // right = array.length -1
+
+    // Iterate through the array, if the number is greater than 0, we don't touch it,
+    // If it's smaller than 0, we push it to the end.
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            array.push(array[i]);
+            array.splice(i,1);
+            i--;
+        }
+    }
+
+
 }
